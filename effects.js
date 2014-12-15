43ba4fcb843f8b73818bb5db4cc2da93c4b83097
nateosher <nateoshe@gmail.com>
2014-12-15 11:49:19 -0500

    Initial Prettying-up
    Started in with javascript/jquery and css stuff to make things not
    ugly/boring

---
+++ effects.js
@@ -0,0 +1,31 @@
+// Effects/ etc.
+
+$(document).ready(function(){
+
+	$('.header').hover(
+		function(){
+			$(this).addClass('change');
+		},
+		function(){
+			$(this).removeClass('change');
+		}
+	);
+
+	$('#box1').hover(
+		function(){
+			$(this).addClass('boxChange');
+		},
+		function(){
+			$(this).removeClass('boxChange');
+		}
+	);
+
+	$('#box3').hover(
+		function(){
+			$(this).addClass('boxChange');
+		},
+		function(){
+			$(this).removeClass('boxChange');
+		}
+	);
+});
