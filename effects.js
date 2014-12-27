5d30b975577d475bbea42d8b785a41542c2a5ed8
Nate Osher <nateoshe@gmail.com>
2014-12-27 16:23:17 -0500

    Aesthetic changes/ sub-page skeletons

--- effects.js
+++ effects.js
@@ -2,15 +2,6 @@
 
 $(document).ready(function(){
 
-	$('.header').hover(
-		function(){
-			$(this).addClass('change');
-		},
-		function(){
-			$(this).removeClass('change');
-		}
-	);
-
 	$('.box1').hover(
 		function(){
 			$(this).addClass('box1Change');
@@ -20,23 +11,12 @@ $(document).ready(function(){
 		}
 	);
 
-	$('.box2').hover(
-		function(){
-			$(this).addClass('box2Change');
-		},
-		function(){
-			$(this).removeClass('box2Change');
-		}
-	);
-
 	$('li').hover(
 		function(){
 			$(this).animate({height: '+=100px'}, 500);
-			$(this).addClass('buttonSelect');
  		},
 		function(){
 			$(this).animate({height: '-=100px'}, 500);
-			$(this).removeClass('buttonSelect');
 		}
 	);
 });
