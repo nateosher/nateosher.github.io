6ff7c88635e61d29c595a6b134ceb982bc977125
Nate Osher <Nateoshe@gmail.com>
2017-02-05 22:37:00 -0600

    Update vanillasub.js to handle non @ phrases

--- js/vanillasub.js
+++ js/vanillasub.js
@@ -6,11 +6,13 @@ document.addEventListener("DOMContentLoaded", function(){
 	var actions = ["study math",
 	"intern",
 	"play music",
+	"make websites",
 	"eat"];
 
 	var locations = ["Carleton College",
 	"DoneGood",
 	"the Cave",
+	undefined,
 	"the dining hall"];
 
 	// Estimate widths
@@ -31,36 +33,49 @@ document.addEventListener("DOMContentLoaded", function(){
 	console.log("start:",widths);
 
 	for(var i = 0; i < locations.length; i++){
-		var locSpan = "<span id='loc" + i.toString() + "' class='stretch'>" + locations[i] + "</span>";
-		document.getElementById("location")
-			.insertAdjacentHTML("afterbegin",locSpan);
-		document.getElementById("loc" + i.toString()).style.display = "none";
-		document.getElementById("loc" + i.toString()).style.opacity = 0;
+		if(locations[i]){
+			var locSpan = "<span id='loc" + i.toString() + "' class='stretch'>" + locations[i] + "</span>";
+			document.getElementById("location")
+				.insertAdjacentHTML("afterbegin",locSpan);
+			document.getElementById("loc" + i.toString()).style.display = "none";
+			document.getElementById("loc" + i.toString()).style.opacity = 0;
+		}
 	}
 
 	function AnimationLoop(){
 		var prev = (counter > 0 ? counter - 1 : actions.length - 1);
-
+		// Hide previous divs
 		Velocity(document.getElementById("ac" + prev.toString()),
 			{ opacity: 0 }, {duration : 1000 });
 		document.getElementById("ac" + prev.toString()).style.display = "none";
-		Velocity(document.getElementById("loc" + prev.toString()),
-			{ opacity: 0 }, {duration : 1000 });
-		document.getElementById("loc" + prev.toString()).style.display = "none";
+		if(locations[prev]){
+			Velocity(document.getElementById("loc" + prev.toString()),
+				{ opacity: 0 }, {duration : 1000 });
+			document.getElementById("loc" + prev.toString()).style.display = "none";			
+		}
 
+		// Show new divs
 		var newWidth = widths[counter]
 		Velocity(document.getElementById("action"),
 			{ width : newWidth }, {duration : 500 });		
 		Velocity(document.getElementById("ac" + counter.toString()),
 			{ opacity: 1 }, {duration : 1500 });
 		document.getElementById("ac" + counter.toString()).style.display = "inline";
-		Velocity(document.getElementById("loc" + counter.toString()),
-			{ opacity: 1 }, {duration : 1500 });
-		document.getElementById("loc" + counter.toString()).style.display = "inline";
+		if(locations[counter]){
+			Velocity(document.getElementById("loc" + counter.toString()), 
+				{ opacity: 1 }, {duration : 1500 });
+			document.getElementById("loc" + counter.toString()).style.display = "inline";
+			document.getElementById("at").style.display = "inline";
+			Velocity(document.getElementById("at"), 
+				{ opacity: 1}, { duration : 1500 });
+		}else{
+			document.getElementById("at").style.opacity = 0;
+			document.getElementById("at").style.display = "none";
+		}
 
 		//TEST
-		widths[counter] = document.getElementById("ac" + counter.toString()).offsetWidth + 5;
-		console.log(widths)
+		// widths[counter] = document.getElementById("ac" + counter.toString()).offsetWidth + 5;
+		// console.log(widths)
 
 		counter = (counter + 1) % actions.length;
 	}
