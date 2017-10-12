eee5a94113a0603be88829e63cf21957a61a479d
Nate Osher <Nateoshe@gmail.com>
2017-10-12 12:38:55 -0400

    Make projects page responsive to window changes

--- js/projectscroll.js
+++ js/projectscroll.js
@@ -6,23 +6,83 @@ document.addEventListener("DOMContentLoaded", function(){
 	var git	 = document.getElementById("gitbutton");
 	var link = document.getElementById("linkedinbutton");
 	var pocket = document.getElementById("PocketPal");
+	var clicker = document.getElementById("ClickGame");
+	var personal = document.getElementById("Personal");
 	var pocketCard = document.getElementById("pocket-card");
 	var clickerCard = document.getElementById("clicker-card");
 	var personalCard = document.getElementById("personal-card");
 
+	var cards = document.querySelectorAll(".card");
+	var colorTexts = document.querySelectorAll(".colorText");
+	var cardHeadings = document.querySelectorAll(".cardHeading");
+	var cardTexts = document.querySelectorAll(".cardText");
+
+	var w=window;
+	var d=document;
+	var e=d.documentElement;
+	var g=d.getElementsByTagName('body')[0];
+	var x=w.innerWidth;
+	var y=w.innerHeight;
+
+	// TODO: Color text assignment HERE 
+
+	pocket.style.height = String(y + 10) + "px";
+	clicker.style.height = String(y + 10) + "px";
+	personal.style.height = String(y + 10) + "px";
+
+	function whenResized(){
+		x=w.innerWidth;
+		y=w.innerHeight;
+		pocket.style.height = String(y + 10) + "px";
+		clicker.style.height = String(y + 10) + "px";
+		personal.style.height = String(y + 10) + "px";
+		for(var i = 0; i < cards.length; i++){
+			cards[i].style.height = "60%";
+			cards[i].style.width = "60%";
+		}
+
+		document.getElementById("projects1").style.height = String(
+												   Math.max((y - 190 - 50),
+												   	350)) + 'px';
+		document.removeEventListener("scroll", onScrollFunction, false);
+		function onScrollFunction(){
+			var yCoord = document.documentElement.scrollTop || 
+							window.pageYOffset;
+			var offset = 0;
+			console.log(yCoord);
+			if(y <= 350){
+				offset = 590;
+			}
+			if(yCoord < 0.4*y + offset){
+				bigify(pocketCard);
+				lightsOn();
+			}else if(yCoord >= 0.4*y + offset && yCoord < 1.4*y + offset){
+				smallify(pocketCard);
+				bigify(clickerCard);
+				bigify(personalCard);
+			}else if(yCoord >= 1.4*y + offset && yCoord < 2.4*y + offset){
+				bigify(pocketCard);
+				smallify(clickerCard);
+				bigify(personalCard);
+			}else if (yCoord >= 2.4*y + offset){
+				lightsOff();
+				bigify(pocketCard);
+				bigify(clickerCard);
+				smallify(personalCard);
+			}
+		}
+	}
+
+	w.onresize = whenResized;
+
 
 	nav.addEventListener("click", function(){
-		Velocity(nav, "scroll", {duration : 500, offset : 55});
-		// Velocity(git, {color : "#ffffff", duration : 200});
-		// Velocity(link, {color : "#ffffff", duration : 200});
-		// Velocity(nav, {border : "#40px solid white", duration : 200});
+		Velocity(pocket, "scroll", {duration : 500});
 	});
 
-	// document.addEventListener('scroll', function(){
-	// 	var yCoord = document.documentElement.scrollTop || 
-	// 					window.pageYOffset;
-	// 	console.log(yCoord);
-	// });
+	document.getElementById("projects1").style.height = String(
+														Math.max((y - 190 - 50),350)) 
+														+ 'px';
 
 	function smallify(card){
 		card.style.padding = "0 0 0 0";
@@ -46,27 +106,32 @@ document.addEventListener("DOMContentLoaded", function(){
 		return 0;
 	}
 
-	document.addEventListener('scroll', function(){
+	function onScrollFunction(){
 		var yCoord = document.documentElement.scrollTop || 
 						window.pageYOffset;
-		if(yCoord < 400){
+		var offset = 0;
+		console.log(yCoord);
+		if(y <= 350){
+			offset = 400;
+		}
+		if(yCoord < 0.4*y + offset){
 			bigify(pocketCard);
 			lightsOn();
-		}else if(yCoord >= 400 && yCoord < 1200){
-			lightsOff();
+		}else if(yCoord >= 0.4*y + offset && yCoord < 1.4*y + offset){
 			smallify(pocketCard);
 			bigify(clickerCard);
 			bigify(personalCard);
-		}else if(yCoord >= 1200 && yCoord < 1800){
-			lightsOff();
+		}else if(yCoord >= 1.4*y + offset && yCoord < 2.4*y + offset){
 			bigify(pocketCard);
 			smallify(clickerCard);
 			bigify(personalCard);
-		}else if (yCoord >= 1800){
+		}else if (yCoord >= 2.4*y + offset){
 			lightsOff();
 			bigify(pocketCard);
 			bigify(clickerCard);
 			smallify(personalCard);
 		}
-	});
+	}
+
+	document.addEventListener('scroll', onScrollFunction, false);
 });
